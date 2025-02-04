79176f6f8ff8511eb26ca45650f2c4cd9756d045
Valera <vl176932@gmail.com>
2025-02-05 05:46:49 +0700

    InterfaceUser

---
+++ Проект по командной работе С++ [Шифр]/Проект по командной работе С++ [Шифр]/Проект по командной работе С++ [Шифр].cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void UserUseMenu() {
+
+    int userInput;
+
+    do {
+        cout << "-------------------------------------" << endl;
+        cout << "Application to encrypt Caesar's words" << endl;
+        cout << "-------------------------------------" << endl;
+
+        cout << "Choose what you want to do" << endl;
+
+        cout << "1. Encrypt using the Caesar method\n"
+             << "2. More complex encryption [Letters to numbers]\n"
+             << "3. Encryption of Viginaire's words\n"
+             << "4. Exit\n";
+
+        cout << "Add: ";
+        cin >> userInput;
+
+        if (userInput == 1) {
+
+        }
+        else if (userInput == 2) {
+
+        }
+        else if (userInput == 3) {
+
+        } 
+        else {
+            cout << "Thank you for using the application" << endl;
+        }
+
+    } while (userInput != 4);
+
+}
+
+int main()
+{
+    UserUseMenu();
+}
+
